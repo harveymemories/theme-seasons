198d22704b1106212a521ef4df949c5a07de5f66
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2018-07-23 12:31:27 -0500

    Remove arabic on advice of usability tester

--- common/footer.php
+++ common/footer.php
@@ -13,7 +13,7 @@
 
 <div id="google_translate_element"></div><script type="text/javascript">
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'ar,es,vi,zh-CN', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+  new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'es,vi,zh-CN', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
 }
 </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
