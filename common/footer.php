431832727b504b2a88899243fba6b375a95398a4
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2018-08-10 10:45:09 -0500

    Put social media links in footer

--- common/footer.php
+++ common/footer.php
@@ -17,7 +17,7 @@ function googleTranslateElementInit() {
 }
 </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
-        <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?> Best viewed with Chrome, Safari, or Firefox.</p>
+        <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>. Follow us on <a href="https://twitter.com/HarveyMemories">Twitter</a> & <a href="https://www.facebook.com/harveymemoriesproject">Facebook</a>.'); ?></p>
 
     <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
 
